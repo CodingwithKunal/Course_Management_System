f570be8c61e55ddebd3962acedae908eeb2a48dd
Kunal Verma <user.email: "kvcoder22@gmail.com>
2026-02-20 22:30:21 +0530

    updated_routes_course

--- Server/src/routes/course.routes.js
+++ Server/src/routes/course.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { authorizeRoles, verifyToken } from '../middleware/auth.middleware.js';
-import { creatCourse ,deleteCourse,getAllCourses, UpdateCourse} from '../controller/course.controller.js';
+import { creatCourse ,deleteCourse,getAllCourses, publishCourse, UpdateCourse,} from '../controller/course.controller.js';
 const router = express.Router();
 
 
@@ -8,5 +8,6 @@ router.post("/create-course", verifyToken, authorizeRoles("INSTRUCTOR"), creatCo
 router.get("/get-courses", verifyToken, authorizeRoles("INSTRUCTOR"), getAllCourses);
 router.put("/update-course/:courseId", verifyToken, authorizeRoles("INSTRUCTOR"), UpdateCourse);
 router.delete("/delete-course/:courseId", verifyToken, authorizeRoles("INSTRUCTOR"), deleteCourse);
+router.get("/get-all-courses", publishCourse); // all courses for students and public
 
 export default router;
