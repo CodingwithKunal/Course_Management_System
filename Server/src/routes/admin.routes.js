34bc058047164bc5fb43ea721b8966b314d046a4
Kunal Verma <user.email: "kvcoder22@gmail.com>
2026-02-27 21:01:55 +0530

    update_admin.routes

--- Server/src/routes/admin.routes.js
+++ Server/src/routes/admin.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { authorizeRoles, authorizeSuperAdmin, verifyToken } from '../middleware/auth.middleware.js';
-import { approveInstructor, blockUnblockUser, getAllUsers, deleteUser , promoteToAdmin } from '../controller/admin.controller.js';
+import { approveInstructor, blockUnblockUser, getAllUsers, deleteUser , promoteToAdmin, publishCourse, getPendingCourses, rejectCourse, unpublishCourse } from '../controller/admin.controller.js';
 const router = express.Router();
 
 
@@ -9,5 +9,9 @@ router.patch("/aprove-instructor/:instructorId", verifyToken, authorizeRoles("AD
 router.patch("/block-unblock-user/:userId", verifyToken, authorizeRoles("ADMIN"), blockUnblockUser);
 router.delete("/delete-user/:userId", verifyToken, authorizeRoles("ADMIN"), deleteUser);
 router.patch("/promote-admin/:userId", verifyToken,authorizeSuperAdmin , authorizeRoles("ADMIN"), promoteToAdmin); 
+router.patch("/publish-course/:courseId", verifyToken, authorizeRoles("ADMIN"), publishCourse);
+router.patch("/reject-course/:courseId", verifyToken, authorizeRoles("ADMIN"), rejectCourse);
+router.patch("/unpublish-course/:courseId", verifyToken, authorizeRoles("ADMIN"), unpublishCourse);
+router.get("/pending-courses", verifyToken , authorizeRoles("ADMIN"), getPendingCourses);
 
 export default router;
