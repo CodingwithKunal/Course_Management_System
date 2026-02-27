39a84a98e63258332f613a36bb56b531df037f99
Kunal Verma <user.email: "kvcoder22@gmail.com>
2026-02-27 20:59:02 +0530

    update_course.controller

--- Server/src/controller/course.controller.js
+++ Server/src/controller/course.controller.js
@@ -1,4 +1,5 @@
 import CourseModel from "../models/course.js";
+import EnrollmentModel from "../models/enrollment.js";
 
 
 export const creatCourse = async (req, res) => {
@@ -81,18 +82,21 @@ export const deleteCourse = async (req, res) => {
     try {
         const { courseId } = req.params;
 
-        const course = await CourseModel.findById(courseId)
-        const deletedCourse = await CourseModel.deleteOne({ _id: courseId });
-        if (!course) {
+        const course = await CourseModel.findById(courseId);
+
+         if (!course) {
             return res.status(404).json({ message: "Course not found" });
         }
-        if (course.deletedCount === 0) {
-            return res.status(404).json({ message: "Course not found" });
-        }
+
         if (course.instructor.toString() !== req.user._id.toString()) {
             return res.status(403).json({ message: "You are not authorized to delete this course" });
         }
+        
+        if (deleteCourse.deletedCount === 0) {
+            return res.status(404).json({ message: "Course not found" });
+        }
 
+        const deletedCourse = await CourseModel.deleteOne({ _id: courseId });
 
 
         res.status(200).json({ message: "Course deleted successfully" }, deletedCourse);
@@ -107,7 +111,7 @@ export const publishCourse = async (req, res) => {
     try {
         const { search, category, level, page = 1, limit = 6 } = req.query //search query for filtering courses by title or category or Level
 
-        const query = { isPublished: true };
+        const query = { status: "PUBLISHED" }; // only fetch published courses 
 
         if (search) {
             query.$or = [
@@ -118,7 +122,7 @@ export const publishCourse = async (req, res) => {
 
         // category ke basis par courses ko filter karna
         if (category) {
-            query.category = { $reges: category, $options: "i" }; // case-insensitive search for category means if user search for "programming" it will match with "Programming" or "programming" in database
+            query.category = { $regex: category, $options: "i" }; // case-insensitive search for category means if user search for "programming" it will match with "Programming" or "programming" in database
         }
 
         // search by level
@@ -160,8 +164,6 @@ export const publishCourse = async (req, res) => {
 
 
 
-
-
 // get single course details for course details page
 export const getCourseById = async (req, res) => {
     try {
@@ -173,7 +175,7 @@ export const getCourseById = async (req, res) => {
             return res.status(404).json({ message: "Course not found" });
         }
 
-        if (!course.isPublished) {
+        if (course.status !== "PUBLISHED") {
             return res.status(403).json({ message: "Course is not published yet" });
         }
 
@@ -184,3 +186,79 @@ export const getCourseById = async (req, res) => {
     }
 }
 
+
+
+
+// enroll in course for students
+export const enrollInCourse = async (req, res) => {
+    try {
+        const { courseId } = req.params;
+        const course = await CourseModel.findById(courseId);
+        if (!course) {
+            return res.status(404).json({ message: "Course not found" });
+        }
+        if (course.status !== "PUBLISHED") {
+            return res.status(403).json({ message: "Course is not published yet" });
+        }
+        const alreadyEnrolled = await EnrollmentModel.findOne({ user: req.user._id, course: courseId });
+        if (alreadyEnrolled) {
+            return res.status(400).json({ message: "You are already enrolled in this course" });
+        }
+        
+        const enrollment = await EnrollmentModel.create({
+            user: req.user._id,
+            course: courseId,
+            paymentStatus: "SUCCESS", // for simplicity we are directly marking payment as success, in real application we will integrate payment gateway and update this status based on payment response
+        })
+
+        res.status(200).json({ message: "Enrolled in course successfully" });
+        
+    } catch (error) {
+        res.status(500).json({message: "Server Error", error: error.message});
+    }
+}
+
+
+
+// see enrolled courses for students
+export const getEnrolledCourses = async (req, res) => {
+    try {
+       const enrollments = await EnrollmentModel.find({ user: req.user._id , paymentStatus:"SUCCESS" }).populate({ path: "course", populate: { path: "instructor", select: "name" } }); // populate course details and instructor name
+
+       if(enrollments.length === 0){
+        return res.status(404).json({ message: "You are not enrolled in any courses" });
+       }
+
+       res.status(200).json({ message: "Enrolled courses fetched successfully", count: enrollments.length, enrollments });
+        
+    } catch (error) {
+        res.status(500).json({message: "Server Error", error: error.message});
+    }
+}
+
+
+
+
+
+export const submitCourseForReview = async (req, res) => {
+    try {
+        const { courseId } = req.params;
+        const course = await CourseModel.findById(courseId);
+        if (!course) {
+            return res.status(404).json({ message: "Course not found" });
+        }
+        if (course.instructor.toString() !== req.user._id.toString()) {
+            return res.status(403).json({ message: "You are not authorized to submit this course for review" });
+        }
+        if (course.status === "PENDING") {
+            return res.status(400).json({ message: "Course is already submitted for review" });
+        }
+        
+        course.status = "PENDING";
+        await course.save();
+        res.status(200).json({ message: "Course submitted for review successfully", course });
+        
+    } catch (error) {
+        res.status(500).json({message: "Server Error", error: error.message});
+    }
+}
