bd8378f66fa389c92014642c2a3480d30bc4e21e
Kunal Verma <user.email: "kvcoder22@gmail.com>
2026-04-16 20:03:15 +0530

    Setup_QueryClientProvider

--- cms-frontend/src/main.jsx
+++ cms-frontend/src/main.jsx
@@ -5,10 +5,15 @@ import App from './App.jsx'
 import { store } from './App/store.js'
 import { Toaster } from 'react-hot-toast'
 import {Provider} from 'react-redux';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <Provider  store={store}>
+  <QueryClientProvider client={queryClient}>
     <App/>
     <Toaster position='top-right'/>
+  </QueryClientProvider>
   </Provider>
 )
