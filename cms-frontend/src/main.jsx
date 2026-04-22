53846ce31c20215bb663b54eff6b8fdabb1bd066
Kunal Verma <user.email: "kvcoder22@gmail.com>
2026-04-22 21:04:52 +0530

    Update Query

--- cms-frontend/src/main.jsx
+++ cms-frontend/src/main.jsx
@@ -4,16 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { store } from './App/store.js'
 import { Toaster } from 'react-hot-toast'
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-  <Provider  store={store}>
-  <QueryClientProvider client={queryClient}>
-    <App/>
-    <Toaster position='top-right'/>
-  </QueryClientProvider>
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <Toaster position='top-right' />
+    </QueryClientProvider>
   </Provider>
 )
