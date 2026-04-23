826abb0b02d63b23673108d599f73abaff129327
Kunal Verma <user.email: "kvcoder22@gmail.com>
2026-04-23 15:11:57 +0530

    main

--- cms-frontend/src/main.jsx
+++ cms-frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { store } from './App/store.js'
-import { Toaster } from 'react-hot-toast'
+import { Toaster } from 'sonner'
 import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
       <App />
-      <Toaster position='top-right' />
+      <Toaster position='top-right' richColors />
     </QueryClientProvider>
   </Provider>
 )
