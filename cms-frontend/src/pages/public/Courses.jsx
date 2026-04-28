f215a5ee3274e1e732cbd0b5bcc35929e53e38b8
Kunal Verma <user.email: "kvcoder22@gmail.com>
2026-04-28 21:09:03 +0530

    Courses

--- cms-frontend/src/pages/public/Courses.jsx
+++ cms-frontend/src/pages/public/Courses.jsx
@@ -18,8 +18,8 @@ function Courses() {
                  />
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5'>
-                {courses.map((course)=>(
-                    <div key={course.id} className='border p-4 rounded-md shadow-sm'>
+                {courses.map((course , key)=>(
+                    <div  key={key} className='border p-4 rounded-md shadow-sm'>
                         <h2 className='text-lg font-semibold'>{course.title}</h2>
                         <p className='text-gray-600'>{course.description}</p>
                         <p className='text-sm text-gray-500'>Category: {course.category}</p>
