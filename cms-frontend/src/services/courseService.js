8787be0ba80ca782944bc597ff8b1cf92be5859c
Kunal Verma <user.email: "kvcoder22@gmail.com>
2026-04-29 21:07:04 +0530

    Course_Service

--- cms-frontend/src/services/courseService.js
+++ cms-frontend/src/services/courseService.js
@@ -1,16 +1,33 @@
 
 import API from "./api"
-import {toast} from "sonner";
+import { toast } from "sonner";
 
 
- export  const getAllCourses = async (params) => {
+
+// Get all courses with pagination and search
+export const getAllCourses = async (params) => {
     try {
-        const res = await API.get("/course/get-all-courses",{ params})
+        const res = await API.get("/course/get-all-courses", { params })
         toast.success(res.data.message)
-        return { ok: true, data: res.data}
+        return { ok: true, data: res.data }
 
     } catch (error) {
         toast.error(error.response?.data?.message || "Failed to fetch courses")
+        return { ok: false }
+    }
+}
+
+
+
+// Get course details by ID 
+export const getCourseDetails = async (coureseId) => {
+    try {
+        const res = await API.get(`/course/course-details/${coureseId}`)
+        toast.success(res.data.message)
+        return { ok: true, data: res.data }
+
+    } catch (error) {
+        toast.error(error.response?.data?.message || "Failed to fetch course details")
         return { ok: false }
     }
 }
