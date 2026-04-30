85967f10e2284870572488df1da093c20177b030
Kunal Verma <user.email: "kvcoder22@gmail.com>
2026-04-30 20:09:50 +0530

    Course_Details

--- cms-frontend/src/pages/public/CourseDetail.jsx
+++ cms-frontend/src/pages/public/CourseDetail.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import { useCourseDetail } from '../../hooks/useCourseDetail.js'
-import { useParams } from 'react-router'
+import { useNavigate, useParams } from 'react-router'
+import { enrollInCourse } from '../../services/courseService.js'
 
 function CourseDetail() {
 
     const { id } = useParams()
+    const Nevigate = useNavigate()
 
     const { course, isLoading, error } = useCourseDetail(id)
 
     if (isLoading) { return <div>Loading...</div>; }
     if (error) { return <div>Error loading course details.</div>; }
     if (!course) { return <div>Course not found.</div>; }
+
+
+
+
+    const handle_Enroll = async () => { 
+        const res = await enrollInCourse(course._id)
+        if(res.ok){
+            Nevigate("/") // Leter we Update 
+        }
+    }
 
 
 
@@ -23,7 +35,7 @@ function CourseDetail() {
             <p>Category: {course.category}</p>
             <p>Price: ${course.price}</p>
 
-            <button>{ course.price === 0 ? "Enroll Now" : "Buy Now" } </button>
+            <button onClick={handle_Enroll} className='border border-white rounded-2xl cursor-pointer py-2 active:bg-amber-300'>{ course.price === 0 ? "Enroll Now" : "Buy Now" } </button>
 
         </div>
     )
