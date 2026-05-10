5a80cbdede48ddc0a2cee30f79614f4aec741e81
Kunal Verma <user.email: "kvcoder22@gmail.com>
2026-05-10 21:46:00 +0530

    Enrollment_Service

---
+++ cms-frontend/src/services/enrollmentService.js
@@ -0,0 +1,46 @@
+import { toast } from 'sonner';
+import API from './api.js';
+
+
+
+export const updateWatchProgress  = async (courseId, currentTime) => {
+    try {
+        const res = await API.patch(`/course/courses/${courseId}/watch-progress`, {currentTime})
+        toast.success(res.data.message)
+        return { ok: true, data: res.data}
+        
+    } catch (error) {
+        toast.error( error.reponse?.data?.message || "Failed to update watch progress. Please try again.")
+        return { ok: false}
+    }
+
+}
+
+
+
+
+export const getCourseProgress = async (courseId) => {
+    try {
+        const res = await API.get(`/course/course/${courseId}/progress`) 
+        toast.success(res.data.message)
+        return {ok: true, data: res.data}
+    } catch (error) {
+        toast.error(error.response?.data?.message || "Failed to fetch course progress. Please try again.")
+        return { ok: false}
+    }
+}
+
+
+
+
+export const markCourseComplete = async (courseId) => {
+    try {
+        const res = await API.patch(`/course/course/${courseId}/complete`)
+        toast.success(res.data.message)
+        return { ok: true, data: res.data}
+
+    }catch (error) {
+        toast.error(error.response?.data?.message || "Failed to mark course as complete. Please try again.")
+        return { ok: false}
+    }
+}
