0abf3cae114b15659b21dad8eabd4f305574125f
Kunal Verma <user.email: "kvcoder22@gmail.com>
2026-05-21 21:52:24 +0530

    Footer

---
+++ cms-frontend/src/components/common/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer () {
+    return (
+        <footer className="text-center  py-5 text-sm text-gray-500">
+            &copy; {new Date().getFullYear()} LMS. All rights reserved.
+        </footer>
+    )
+}
+export default Footer
