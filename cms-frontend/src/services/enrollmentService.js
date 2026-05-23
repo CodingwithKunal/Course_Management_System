a34731246fe69a2d066ad4e73640ce2861a3ef16
Kunal Verma <user.email: "kvcoder22@gmail.com>
2026-05-23 21:00:02 +0530

    EnrollmentService

--- cms-frontend/src/services/enrollmentService.js
+++ cms-frontend/src/services/enrollmentService.js
@@ -1,17 +1,18 @@
 import { toast } from 'sonner';
 import API from './api.js';
+import { data } from 'react-router';
 
 
 
-export const updateWatchProgress  = async (courseId, currentTime) => {
+export const updateWatchProgress = async (courseId, currentTime) => {
     try {
-        const res = await API.patch(`/course/courses/${courseId}/watch-progress`, {currentTime})
+        const res = await API.patch(`/course/courses/${courseId}/watch-progress`, { currentTime })
         toast.success(res.data.message)
-        return { ok: true, data: res.data}
-        
+        return { ok: true, data: res.data }
+
     } catch (error) {
-        toast.error( error.reponse?.data?.message || "Failed to update watch progress. Please try again.")
-        return { ok: false}
+        toast.error(error.reponse?.data?.message || "Failed to update watch progress. Please try again.")
+        return { ok: false }
     }
 
 }
@@ -21,12 +22,12 @@ export const updateWatchProgress  = async (courseId, currentTime) => {
 
 export const getCourseProgress = async (courseId) => {
     try {
-        const res = await API.get(`/course/course/${courseId}/progress`) 
+        const res = await API.get(`/course/course/${courseId}/progress`)
         toast.success(res.data.message)
-        return {ok: true, data: res.data}
+        return { ok: true, data: res.data }
     } catch (error) {
         toast.error(error.response?.data?.message || "Failed to fetch course progress. Please try again.")
-        return { ok: false}
+        return { ok: false }
     }
 }
 
@@ -37,10 +38,24 @@ export const markCourseComplete = async (courseId) => {
     try {
         const res = await API.patch(`/course/course/${courseId}/complete`)
         toast.success(res.data.message)
-        return { ok: true, data: res.data}
+        return { ok: true, data: res.data }
 
-    }catch (error) {
+    } catch (error) {
         toast.error(error.response?.data?.message || "Failed to mark course as complete. Please try again.")
-        return { ok: false}
+        return { ok: false }
+    }
+}
+
+
+
+
+export const getMyEnrollments = async () => {
+    try {
+        const res = await API.get("/course/my-enrollments")
+        toast.success(res.data.message)
+        return { ok: true, data: res.data }
+    } catch (error) {
+        toast.error(error.response?.data?.message || "Failed to fetch enrollments. Please try again.")
+        return { ok: false }
     }
 }
