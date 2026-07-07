2152c8777b26a39ee9fda0dd660c92563a33113f
Kunal Verma <user.email: "kvcoder22@gmail.com>
2026-07-07 21:21:37 +0530

    Use_AdminData

---
+++ cms-frontend/src/hooks/useAdminData.js
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { getAllUsers, getPending_course } from "../services/adminService";
+
+
+export const adminData = () => {
+
+    const usersQuery = useQuery({
+        queryKey: ["admin_user"],
+        queryFn: getAllUsers,
+    });
+
+    const coursesQuery = useQuery({
+        queryKey: ["admin_courses"],
+        queryFn: getPending_course,
+    });
+
+    return {
+        users: usersQuery.data?.data?.users || [],
+
+        pending_courses: coursesQuery.data?.data?.pending_courses || [],
+
+        userLoading: usersQuery.isLoading,
+        courseLoading: coursesQuery.isLoading,
+
+        refetchUsers: usersQuery.refetch,
+        refetchCourse: coursesQuery.refetch,
+
+    };
+
+};
