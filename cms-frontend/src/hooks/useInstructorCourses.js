295da60babe79e4709ee150407ea85198082b544
Kunal Verma <user.email: "kvcoder22@gmail.com>
2026-08-04 11:35:16 +0530

    Instructor_courses

--- cms-frontend/src/hooks/useInstructorCourses.js
+++ cms-frontend/src/hooks/useInstructorCourses.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query"
-import { getInstructorCourses } from "../services/instructorService.js"
+import { getInstructorCourses, getEnrollmentsForInstructor, getRevenueForInstructor } from "../services/instructorService.js"
 
 export const useInstructorCourse = () => {
     const { data, error, isLoading, refetch} = useQuery({
@@ -13,6 +13,34 @@ return {
     isLoading,
     refetch,
    
-
 }
+}
+
+
+export const getInstructorEnrollments = () => {
+    const { data, error, isLoading, refetch} = useQuery({
+        queryKey:["getInstructor_enrollments"],
+        queryFn: () => getEnrollmentsForInstructor(),
+    })
+    return {
+        enrollments: data?.data?.totalEnrollments || 0,
+        error,
+        isLoading,
+        refetch,
+    }
+}
+
+
+
+export const getInstructorRevenue = () => {
+    const { data, error, isLoading, refetch} = useQuery({
+        queryKey:["getInstructor_revenue"],
+        queryFn: () => getRevenueForInstructor(),
+    })
+    return {
+        revenue: data?.data?.totalRevenue || 0,
+        error,
+        isLoading,
+        refetch,
+    }
 }
