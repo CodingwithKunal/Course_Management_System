66b881ecd4347008baefa4680bfd84604cd92e6b
Kunal Verma <user.email: "kvcoder22@gmail.com>
2026-09-14 21:21:11 +0530

    Use_Courses

--- cms-frontend/src/hooks/useCourses.js
+++ cms-frontend/src/hooks/useCourses.js
@@ -1,25 +1,31 @@
 import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { getAllCourses } from "../services/courseService.js";
-
+import { useDebounce } from "./useCourse_Debounce_search.js";
 
 
 export const useCourses = () => {
     const [filters, setfilters] = useState({
-        category: "",
-        level: "",
-        search:"",
-        page: 1, 
+        category:[],
+        level:[],
+        search: "",
+        page: 1,
     });
-    
+  
+    const debouncedSearch = useDebounce(filters.search, 500)
 
     const { data, isLoading, isError, refetch } = useQuery({
-        queryKey: ["courses", filters], 
-        queryFn: () => getAllCourses(filters)
+        queryKey: ["courses", { ...filters, search: debouncedSearch }],
+        queryFn: () => getAllCourses({ ...filters, search: debouncedSearch }),
+
+        // Naya data aane tak purana data screen par hold karke rakhega
+        placeholderData: keepPreviousData,
     });
     return {
         courses: data?.data?.courses || [],
         totalPages: data?.data?.totalPages || 1,
+        currentPage: data?.data?.currentPage || 1,
+        totalCourses: data?.data?.totalCourses || 0,
         isLoading,
         isError,
         setfilters,
